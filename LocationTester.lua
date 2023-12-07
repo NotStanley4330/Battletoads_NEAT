25ffd371ddd29300847b83721cf8ed2f22265e89
NotStanley4330 <michaelbrian.norris82@gmail.com>
2023-12-07 12:04:03 -0700

    makign sure it doesnt overload

--- LocationTester.lua
+++ LocationTester.lua
@@ -3,9 +3,26 @@
 --Purpose: Help me track down what exactly are the values
 -- that are being held and tracked in the pos variables in
 --Battletoads NES, I will attempt to document them as I slowly figure it out
+Objects_Xpos_old = 0
+player2_Xpos_old = {}
+for slot=0,12 do
+    player2_Xpos_old[#player2_Xpos_old+1] = 0
+end
+Object14_Xpos_old = 0
+Sprites_Xpos_old = 0
+sprite2_Xpos_old = {}
+for slot=0,13 do
+    sprite2_Xpos_old[#sprite2_Xpos_old+1] = 0
+end
+
 while true do
     Objects_Xpos_H = memory.readbyte(0x03EE)
-    print(string.format("Objects_Xpos_H is: %d", Objects_Xpos_H))
+    if (Objects_Xpos_old ~= Objects_Xpos_H) then
+        print(string.format("Objects_Xpos_H is: %d", Objects_Xpos_H))
+        Objects_Xpos_old = Objects_Xpos_H
+    end
+
+
 
     player2_Xpos_H = {}
     for slot=0,12 do
@@ -14,9 +31,9 @@ while true do
         player2_Xpos_H[#player2_Xpos_H+1] = temp_x
     end
     
-    print("player2_Xpos_H:")
+    --print("player2_Xpos_H:")
     for slot=0,12 do --It appears to be 13 bytes long so this should cover the whole thing
-        if (player2_Xpos_H[slot] == nil) then
+        if (player2_Xpos_H[slot] == nil and (player2_Xpos_old[slot] ~= player2_Xpos_H[slot])) then
             print(string.format("The value of player2_Xpos_H in slot %d is NIL!", slot))
         else
             print(string.format("The value of player2_Xpos_H in slot %d is: %d", slot, player2_Xpos_H[slot]))
@@ -49,7 +66,7 @@ while true do
 
 
 
-
-
+    --this is unportable as heck but I dont have a better solution atm
+    os.execute("sleep 1")
 
 end
--- NEATEvolve.lua
+++ NEATEvolve.lua
@@ -1082,35 +1082,35 @@ end
 
 function writeFile(filename)
         local file = io.open(filename, "w")
-	file:write(pool.generation .. "\n")
-	file:write(pool.maxFitness .. "\n")
-	file:write(#pool.species .. "\n")
+		file:write(pool.generation .. "\n")
+		file:write(pool.maxFitness .. "\n")
+		file:write(#pool.species .. "\n")
         for n,species in pairs(pool.species) do
-		file:write(species.topFitness .. "\n")
-		file:write(species.staleness .. "\n")
-		file:write(#species.genomes .. "\n")
-		for m,genome in pairs(species.genomes) do
-			file:write(genome.fitness .. "\n")
-			file:write(genome.maxneuron .. "\n")
-			for mutation,rate in pairs(genome.mutationRates) do
-				file:write(mutation .. "\n")
-				file:write(rate .. "\n")
-			end
-			file:write("done\n")
-			
-			file:write(#genome.genes .. "\n")
-			for l,gene in pairs(genome.genes) do
-				file:write(gene.into .. " ")
-				file:write(gene.out .. " ")
-				file:write(gene.weight .. " ")
-				file:write(gene.innovation .. " ")
-				if(gene.enabled) then
-					file:write("1\n")
-				else
-					file:write("0\n")
+			file:write(species.topFitness .. "\n")
+			file:write(species.staleness .. "\n")
+			file:write(#species.genomes .. "\n")
+			for m,genome in pairs(species.genomes) do
+				file:write(genome.fitness .. "\n")
+				file:write(genome.maxneuron .. "\n")
+				for mutation,rate in pairs(genome.mutationRates) do
+					file:write(mutation .. "\n")
+					file:write(rate .. "\n")
+				end
+				file:write("done\n")
+				
+				file:write(#genome.genes .. "\n")
+				for l,gene in pairs(genome.genes) do
+					file:write(gene.into .. " ")
+					file:write(gene.out .. " ")
+					file:write(gene.weight .. " ")
+					file:write(gene.innovation .. " ")
+					if(gene.enabled) then
+						file:write("1\n")
+					else
+						file:write("0\n")
+					end
 				end
 			end
-		end
         end
         file:close()
 end
