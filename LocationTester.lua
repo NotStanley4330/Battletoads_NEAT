731098f2f508790430b2611399ebfc3f707f2e62
NotStanley4330 <michaelbrian.norris82@gmail.com>
2023-12-07 12:24:25 -0700

    blah

--- LocationTester.lua
+++ LocationTester.lua
@@ -8,14 +8,17 @@ player2_Xpos_old = {}
 for slot=0,12 do
     player2_Xpos_old[#player2_Xpos_old+1] = 0
 end
+
 Object14_Xpos_old = 0
 Sprites_Xpos_old = 0
 sprite2_Xpos_old = {}
+
 for slot=0,13 do
     sprite2_Xpos_old[#sprite2_Xpos_old+1] = 0
 end
 
 while true do
+
     Objects_Xpos_H = memory.readbyte(0x03EE)
     if (Objects_Xpos_old ~= Objects_Xpos_H) then
         print(string.format("Objects_Xpos_H is: %d", Objects_Xpos_H))
@@ -35,16 +38,25 @@ while true do
     for slot=0,12 do --It appears to be 13 bytes long so this should cover the whole thing
         if (player2_Xpos_H[slot] == nil and (player2_Xpos_old[slot] ~= player2_Xpos_H[slot])) then
             print(string.format("The value of player2_Xpos_H in slot %d is NIL!", slot))
-        else
+        elseif (player2_Xpos_old[slot] ~= player2_Xpos_H[slot]) then
+            --blah
             print(string.format("The value of player2_Xpos_H in slot %d is: %d", slot, player2_Xpos_H[slot]))
         end
+        player2_Xpos_old[slot] = player2_Xpos_H[slot]
     end
 
     Object14_Xpos_H = memory.readbyte(0x03FC)
-    print(string.format("Object14_Xpos_H is: %d", Object14_Xpos_H))
+    if (Object14_Xpos_H ~= Object14_Xpos_old) then 
+        print(string.format("Object14_Xpos_H is: %d", Object14_Xpos_H))
+        Object14_Xpos_old = Object14_Xpos_H
+    end
 
     Sprites_Xpos = memory.readbyte(0x203)
-    print(string.format("Sprites_Xpos is: %d", Sprites_Xpos))
+    if (Sprites_Xpos_old ~= Sprites_Xpos) then
+        print(string.format("Sprites_Xpos is: %d", Sprites_Xpos))
+        Sprites_Xpos_old = Sprites_Xpos
+    end
+    
 
     sprite2_Xpos = {}
     for slot=0,13 do
@@ -53,13 +65,15 @@ while true do
     end
 
     --print the values of sprite2_Xpos
-    print("sprite2_Xpos")
+    --print("sprite2_Xpos")
     for slot=0,13 do
-        if (sprite2_Xpos[slot] == nil) then
+        if (sprite2_Xpos[slot] == nil and (sprite2_Xpos_old[slot] ~= sprite2_Xpos_H[slot])) then
             print(string.format("The value of sprite2_Xpos at slot %d is: NIL!", slot))
-        else
+        elseif (sprite2_Xpos_old[slot] ~= sprite2_Xpos_H[slot]) then
+            --blah
             print(string.format("The value of sprite2_Xpos at slot %d is: %d", slot, sprite2_Xpos[slot]))
         end
+        sprite2_Xpos_old[slot] = sprite2_Xpos_H[slot]
     end
 
 
@@ -67,6 +81,5 @@ while true do
 
 
     --this is unportable as heck but I dont have a better solution atm
-    os.execute("sleep 1")
 
 end
